eae52e3d4db1315c0594409c6d73d34619328b06
Rajeev Rawat <rajeevrawat2100@email.com>
2026-07-16 13:31:22 +0530

    initialized setup and bootstrap dependencies is installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-   
-
   return (
-   <>
-    <h1>Hello world</h1>
-   </>
+    <>
+      <Header />
+      <main className="container py-3">
+        <h1 className="bg-danger">Hello world</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
